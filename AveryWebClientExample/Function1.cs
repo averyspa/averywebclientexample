dddca3a7e98ed605a8bd7391cf2cd8412478f1c6
sordahl-ga <sordahl@bellsouth.net>
2019-03-29 15:11:36 -0400

    Initial

--- AveryWebClientExample/Function1.cs
+++ AveryWebClientExample/Function1.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net;
+using System.Text;
 
 namespace AveryWebClientExample
 {
@@ -14,20 +16,45 @@ namespace AveryWebClientExample
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            // Create a request using a URL that can receive a post. 
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(System.Environment.GetEnvironmentVariable("https://www.corportateamerica.com/api/...."));
+            // Set the Method property of the request to POST.
+            request.Method = "POST";
+            // Create POST data and convert it to a byte array.
+            request.Headers.Add("Content-Type:x-www-form-urlencoded;charset=UTF-8");
+            request.Headers.Add("Authorization:Zoho-authtoken <token>");
+            //Encode
+            string stringData = "JSONString=\"" + requestBody + "\"";
+            var data = Encoding.UTF8.GetBytes(stringData);
+            request.ContentLength = data.Length;
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                // Write the data to the request stream.
+                dataStream.Write(data, 0, data.Length);
+            }
+            // Get the response.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            // Display the status.
+            //Console.WriteLine(response.StatusDescription);
+            // Get the stream containing content returned by the server.
+            string responseFromServer = "";
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    // Read the content.
+                    responseFromServer = reader.ReadToEnd();
+                }
+            }
+            return new OkObjectResult(responseFromServer);
+
         }
     }
 }
